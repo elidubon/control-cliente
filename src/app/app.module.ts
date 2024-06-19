dd4e9b950f2eeac20266e0c52f421c80f94d8bf6
Dubons <rosaliodubon@gmail.com>
2024-06-18 21:05:11 -0600

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './componente/cabecero/cabecero.component';
+import { TableroComponent } from './componente/tablero/tablero.component';
+import { ClientesComponent } from './componente/clientes/clientes.component';
+import { EditarClienteComponent } from './componente/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './componente/login/login.component';
+import { RegistroComponent } from './componente/registro/registro.component';
+import { ConfiguracionComponent } from './componente/configuracion/configuracion.component';
+import { NoEncontradoComponent } from './componente/no-encontrado/no-encontrado.component';
+import { PiePaginaComponent } from './componente/pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    TableroComponent,
+    ClientesComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    RegistroComponent,
+    ConfiguracionComponent,
+    NoEncontradoComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
